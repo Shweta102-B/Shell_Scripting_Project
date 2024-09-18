a926aaf0fa4656891cce86c660ca43bb61fd037a
Ubuntu <ubuntu@ip-172-31-32-54.ec2.internal>
2024-09-18 14:08:23 +0000

    simple-shell-script

---
+++ Project_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###########################
+#Author: Shweta Bhowate
+
+#Date:-18-09-2024
+
+#########################
+
+#create a new directory if not exits
+mkdir users_login_date 
+
+#Get Date in a variable
+DATE=$(date +'%m-%d-%Y-%H-%M')
+
+#log the users Data ina file
+who | awk '{print $1,$3,$4,$5}' > users_login_date/Users_$DATE.txt
