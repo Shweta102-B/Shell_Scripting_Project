0e23d8fa7135c0a91b8765bfccb692f0cc17e37b
Ubuntu <ubuntu@ip-172-31-32-54.ec2.internal>
2024-09-18 14:38:09 +0000

    minor fixes

--- Project_1.sh
+++ Project_1.sh
@@ -13,5 +13,5 @@ mkdir users_login_date
 #Get Date in a variable
 DATE=$(date +'%m-%d-%Y-%H-%M')
 
-#log the users Data ina file
+#log the users Data in a file
 who | awk '{print $1,$3,$4,$5}' > users_login_date/Users_$DATE.txt
