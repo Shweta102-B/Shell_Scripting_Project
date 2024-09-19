c27f5271be8fad162dad9f10fc6f104476062f6b
Ubuntu <ubuntu@ip-172-31-32-54.ec2.internal>
2024-09-19 02:02:33 +0000

    list of users

--- list_resources/aws_resource_tracker.sh
+++ list_resources/aws_resource_tracker.sh
@@ -11,23 +11,28 @@
 ##
 ## AWS S3    # AWS Ec2   # AWS Lambda  # AWS IAM Users
 #
- mkdir list_resources
 
  set -x
 
 # #list s3 buckets
    echo "Print the list of s3 buckets" 
-   aws s3 ls   > list_resources/resource.txt
+   aws s3 ls   >> resource.txt
 #
 #  #list EC2 Instances
    echo "Print the list of ec2 Instances"
-   aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >>list_resources/resource.txt
+   aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >> resource.txt
 
-   
+
+   #list the list of vpcs
+   echo "Print the list of vpcs"
+   aws ec2 describe-vpcs | jq '.Vpcs[].VpcId'  >> resource.txt
+
+
   #list lambda functions
    echo "Print the list of ec2 Instances"
-   aws lambda list-functions  >> list_resources/resource.txt
+   aws lambda list-functions  >> resource.txt
 #
    #list IAM users
    echo "IAM users"
-   aws iam list-users   >> list_resources/resource.txt
+   aws iam list-users   >> resource.txt
+
